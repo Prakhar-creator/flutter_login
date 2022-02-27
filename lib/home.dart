e670cadd5317f42bafeaea9b307485fd67befa04
Prakhar Creator <prakharmishra288@gmail.com>
2022-02-27 23:20:27 +0530

    da- 8 completed

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'dart:convert';
+
 import 'package:flutter_login/models/catalog.dart';
 import 'package:flutter_login/widgets/drawer.dart';
 import 'package:flutter_login/widgets/item_widget.dart';
@@ -25,24 +27,30 @@ class _homeState extends State<home> {
         await rootBundle.loadString("assets/files/catalog.json");
     final decodeData = jsonDecode(catalogJson);
     var productsData = decodeData["products"];
-    print(productsData);
+    catalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate(1, (index) => catalogModel.items[0]);
     return Scaffold(
         appBar: AppBar(
           title: Text('Catalogue App'),
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: ListView.builder(
-            itemCount: dummylist.length,
-            itemBuilder: (BuildContext context, int index) {
-              return ItemWidget(item: catalogModel.items[index]);
-            },
-          ),
+          child: (catalogModel.items != null && catalogModel.items!.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: catalogModel.items!.length,
+                  itemBuilder: (context, index) => ItemWidget(
+                    item: catalogModel.items![index],
+                  ),
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                ),
         ),
         drawer: MyDrawer());
   }
